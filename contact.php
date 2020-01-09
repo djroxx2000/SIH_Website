1f75c43bdc79bfa7b01252f14800d8cb5d6d217c
hritikkothari <kotharihrithik1@gmail.com>
2020-01-09 19:30:38 +0530

    merged everythin

--- contact.php
+++ contact.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+<?php require_once("includes/header.php"); ?>
+
     <div class="container">
         <form action="action_page.php">
 
@@ -24,7 +15,6 @@
           <input type="submit" value="Submit">
 
         </form>
-      </div>
+    </div>
 
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>
--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Court Case Management System</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <script src="https://kit.fontawesome.com/2a6e9d1db8.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="public/css/style.css">
-  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-  <link rel = "icon"
-    href = "public/images/statue.jpg"
-    type = "image/x-icon">
-
-</head>
-<body>
+<?php require_once("includes/header.php"); ?>
 
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">
@@ -23,35 +7,24 @@
         </a>
     </nav>
 
-    <div id="mySidepanel" class="sidepanel">
-      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-      <a href="#">About</a>
-      <a href="#">Services</a>
-      <a href="#">Clients</a>
-      <a href="#">Contact</a>
-    </div>
-
-    <button class="openbtn" onclick="openNav()">&#9776; </button>
-
     <div class="split left">
-      <div class="centered">
-        <p>"No one is above the law and no one is below the law.Justice has got to be blind.The more powerful shall not pickon the less powerful."
-            <span id="quote">
-                ~Josh Earnest
-            </span>
-        </p>
-      </div>
+        <div class="centered">
+            <p>"No one is above the law and no one is below the law.Justice has got to be blind.
+                The more powerful shall not pick on the less powerful."
+                <span id="quote">
+                    ~Josh Earnest
+                </span>
+            </p>
+            <a href="contact.php" class="btn btn-info" role="button">
+                Contact us
+            </a>
+        </div>
     </div>
 
     <div class="split right">
-      <div class="centered">
-        <img src="public/images/statue.jpg" alt="statue">
-      </div>
+        <div class="centered">
+            <img src="public/images/statue.jpg" alt="statue">
+        </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>
