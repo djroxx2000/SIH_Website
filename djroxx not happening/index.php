5e4b930d0b1dcbcd369a9149c659e11d48010004
Dhananjay Shettigar <39980717+djroxx2000@users.noreply.github.com>
2020-01-17 12:01:32 +0530

    Hello from the other side
    Lawyer stuff, some db stuff and some index stuff

---
+++ djroxx not happening/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Court DBMS</title>
+    <link rel="stylesheet" href="public/css/carousel.css">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="https://kit.fontawesome.com/2f7569df82.js" crossorigin="anonymous"></script>
+    <link rel = "icon"
+    href = "public/images/statue.jpg"
+    type = "image/x-icon">
+</head>
+
+<body>
+
+    <!--Vertical Left in-page Nav Bar-->
+    <div>
+        <ul>
+            <li class="navbar" title="Home" onclick="location.href = '#home' ">
+                <i class="fab fa-jira fa-2x" id="home-btn"></i>
+            </li>
+            <li class="navbar" title="Login/Signup" onclick="location.href = '#login' ">
+                <i class="fab fa-jira fa-2x" id="login-btn"></i>
+            </li>
+            <li class="navbar" title="FAQ" onclick="location.href = '#faq' ">
+                <i class="fab fa-jira fa-2x" id="faq-btn"></i>
+            </li>
+            <li class="navbar" title="Contact" onclick="location.href = '#contact' ">
+                <i class="fab fa-jira fa-2x" id="contact-btn"></i>
+            </li>
+        </ul>
+    </div>
+    <!-- left nav ends-->
+
+    <div id="home">
+        <div class="title-bar">
+            <div id="item1">
+                <i class="fas fa-gavel fa-3x"></i>
+                <h1>Court Case Information System</h1>
+            </div>
+            <div id="item2">
+                <button class="sm" id="home-login-btn" onclick="location.href='#login'">Login</button>
+            </div>
+        </div>
+        <div class="index">
+            <div class="left">
+                <p class="quote">"No one is above the law and no one is below the law. Justice has got to be blind. The
+                    more powerful shall not pick on the less powerful."
+                    <br><br>
+                    <span id="quote1">
+                        ~ Josh Earnest
+                    </span>
+                </p>
+            </div>
+
+            <div class="right">
+                <img src="public/images/statue.jpg" alt="statue" class="justice">
+            </div>
+        </div>
+    </div>
+    <!-- home section ends -->
+
+    <!--public/images/quote2.jpg
+    onclick="'
+    onclick="window.location.href='lawyer_login.php'
+    onclick="window.location.href='admin_login.php'-->
+    <div id="login">
+        <div class="carousel-wrapper">
+            <div class="carousel__button carousel__button--prev hide">
+                <i class="fas fa-chevron-left fa-4x"></i>
+            </div>
+            <div class="carousel__track-container">
+                <ul class="carousel__track">
+                    <li class="carousel__slide current-slide">
+                        <img class="carousel__image" src="public/images/quote1.jpg" alt="">
+                        <button class="sm" id="client-btn" onclick="window.location.href='client_login.php'">
+                            Log in as Client
+                        </button>
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__image" src="public/images/quote2.jpg" alt="">
+                        <button class="sm" id="lawyer-btn" onclick="window.location.href='lawyer_login.php'">
+                            Log in as Lawyer
+                        </button>
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__image" src="public/images/quote3.jpg" alt="">
+                        <button class="sm" id="admin-btn" onclick="window.location.href='admin_login.php'">
+                            Log in as Admin
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            <div class="carousel__button carousel__button--next">
+                <i class="fas fa-chevron-right fa-4x"></i>
+            </div>
+        </div>
+        <div class="carousel__nav">
+            <button class="carousel__indicator current-slide"></button>
+            <button class="carousel__indicator"></button>
+            <button class="carousel__indicator"></button>
+        </div>
+    </div>
+    <!-- login carousel ends -->
+
+    <div id="faq">
+        <div class="faq-content">
+            <h3>Frequently asked Questions</h3>
+
+            <button class="accordion">Question 1 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <button class="accordion">Question 2 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <button class="accordion">Question 3 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <button class="accordion">Question 4 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <button class="accordion">Question 5 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <button class="accordion">Question 6 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+
+            <button class="accordion">Question 7 ?</button>
+            <div class="panel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+        </div>
+    </div>
+    <!-- faq section ends -->
+
+    <script src="public/js/app.js"> </script>
+
+</body>
+</html>
+
+
+
+
+
+<?php
+//if logged in, will redirected to dashobard
+if(isset($_SESSION['user'])){
+    echo '<script>window.location.replace("clientdashboard.php");</script>';
+}
+//Logout Script
+if(isset($_GET['logout'])){
+    session_destroy();
+    unset($_SESSION);
+    echo '<script>window.location.replace("index.php");</script>';
+}
+
+?>
